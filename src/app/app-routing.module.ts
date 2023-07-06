2990e0fd83a3f8ee949db8f14f366d64f961fdd0
juan david rios <juandavid3196@gmail.com>
2023-07-06 18:48:25 -0500

    Welcome page, 404 page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './components/welcomePage/welcome-page/welcome-page.component';
 
 const routes: Routes = [
+{path:'', component:WelcomePageComponent},  
 {path:'board',loadChildren: () => import('./modules/board/board.module').then(m=>m.BoardModule)},
 {path:'**',component:PageNotFoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
+import { WelcomePageComponent } from './components/welcomePage/welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     PageNotFoundComponent,
+    WelcomePageComponent,
     
   ],
   imports: [
